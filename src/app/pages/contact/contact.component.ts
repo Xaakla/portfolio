42f32fc298aac628efec680e456116f0cf02ab5e
srrocha <diego.rb0307@gmail.com>
2021-05-18 21:52:22 -0300

    contact me screen styled up

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,11 +8,24 @@ import { Title } from '@angular/platform-browser';
 })
 export class ContactComponent implements OnInit {
 
+  @Input() username;
+
   constructor(private titleService: Title) {
     this.titleService.setTitle("Xaakla - Contact Me");
   }
 
   ngOnInit() {
   }
+
+  expand(lbl){
+    let inputElem = document.querySelector(`#${lbl}`);
+    let labelElem = document.querySelector(`#lbl-${lbl}`);
+    console.log(inputElem, labelElem)
+    console.log(typeof inputElem)
+    inputElem.style.height = "45px";
+    inputElem.classList.add("my-style");
+    labelElem.style.transform = "translateY(-45px)";
+  }
+
 
 }
